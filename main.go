c8c4fd1487a064ece06b1d7a33ac371b206bc0a5
jihonghe <1654508502@qq.com>
2022-01-28 15:31:01 +0800

    initial main

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"net/http"
+
+	"go-web-framework/summer"
+)
+
+func main() {
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: summer.NewCore(),
+	}
+
+	server.ListenAndServe()
+}
--- summer/core.go
+++ summer/core.go
@@ -1,1 +1,17 @@
-package framework
+package summer
+
+import (
+	"net/http"
+)
+
+// Core 框架核心结构体
+type Core struct{}
+
+// NewCore 初始化 Core 对象
+func NewCore() *Core {
+	return &Core{}
+}
+
+func (c Core) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// TODO: hello
+}
